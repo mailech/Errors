09dca2d290118270adb98c484aadd973eedf9371
SUBHASH <subha11.11raj@gmail.com>
2025-09-20 17:01:03 +0530

    Sentinel: auto-fix

--- buggy_math.py
+++ buggy_math.py
@@ -3,7 +3,7 @@ def calculate_total(items):
     total = 0
     for item in items:
         # BUG: Should be addition, not subtraction
-        total = total - item['price']
+        total = total + item['price']
     return total
 
 def divide_numbers(a, b):
@@ -19,7 +19,7 @@ def find_max(numbers):
     max_val = numbers[0]
     for num in numbers:
         # BUG: Should be > not <
-        if num < max_val:
+        if num > max_val:
             max_val = num
     return max_val
 
@@ -30,9 +30,9 @@ def calculate_average(numbers):
     
     total = sum(numbers)
     # BUG: Should divide by len(numbers), not len(numbers) + 1
-    return total / (len(numbers) + 1)
+    return total / len(numbers)
 
 def is_even(number):
     """Check if number is even - HAS BUG"""
     # BUG: Should be % 2 == 0, not % 2 == 1
-    return number % 2 == 1
+    return number % 2 == 0
